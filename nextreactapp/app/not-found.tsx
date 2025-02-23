72bdf08c9ab0afc70cdfe41ca142ffa998900394
niroop893 <niroop893@gmail.com>
2025-02-23 11:51:28 +0530

    nesting folders

--- nextreactapp/app/not-found.tsx
+++ nextreactapp/app/not-found.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function NotFound() {
   return (
     <div>
-      <h1>404</h1>
+      <h1>Page Not Found</h1>
+      <h2>Resource Not Found</h2>
     </div>
   )
 }
